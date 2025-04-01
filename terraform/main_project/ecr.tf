a1ef2d7470d3333158cef63582bd9ad01289d3b3
Artsiom-Kapset <Artsiom.Kapset@puregym.com>
2025-04-01 13:55:43 +0200

    feat: ECR lifecycle policy is added

--- terraform/main_project/ecr.tf
+++ terraform/main_project/ecr.tf
@@ -5,3 +5,52 @@ resource "aws_ecr_repository" "ecr" {
 output "repository_url" {
   value = aws_ecr_repository.ecr.repository_url
 }
+
+resource "aws_ecr_lifecycle_policy" "ecr_policy" {
+  repository = aws_ecr_repository.ecr.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep latest image only"
+        selection = {
+          tagStatus     = "tagged"
+          tagPrefixList = ["latest"]
+          countType     = "imageCountMoreThan"
+          countNumber   = 1
+        }
+        action = {
+          type = "retain"
+        }
+      },
+      {
+        rulePriority = 2
+        description  = "Remove all other tagged images after 7 days"
+        selection = {
+          tagStatus     = "tagged"
+          tagPrefixList = [""]
+          countType     = "sinceImagePushed"
+          countUnit     = "days"
+          countNumber   = 7
+        }
+        action = {
+          type = "expire"
+        }
+      },
+      {
+        rulePriority = 3
+        description  = "Remove untagged images after 7 days"
+        selection = {
+          tagStatus   = "untagged"
+          countType   = "sinceImagePushed"
+          countUnit   = "days"
+          countNumber = 7
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
